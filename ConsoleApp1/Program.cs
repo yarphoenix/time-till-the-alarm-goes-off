2c12d6bac5b00137a28c85c961bf8f359368592a
yarik <yarik@PHOENIXWRIGHT->
2025-03-23 15:14:48 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] TimeToBedStr = Console.ReadLine().Split(':');
+            int[] TimeToBed = new int[TimeToBedStr.Length];
+
+            string[] TimeToWakeUpStr = Console.ReadLine().Split(':');
+            int[] TimeToWakeUp = new int[TimeToWakeUpStr.Length];
+
+            for (int i = 0; i < TimeToBedStr.Length; i++)
+            {
+                int time = int.Parse(TimeToBedStr[i]);
+                TimeToBed[i] = time;
+
+                int time1 = int.Parse(TimeToWakeUpStr[i]);
+                TimeToWakeUp[i] = time1;
+            }
+
+            DateTime now = DateTime.Now;
+
+            DateTime start = new DateTime(now.Year, now.Month, now.Day, TimeToBed[0], TimeToBed[1], 0);
+            DateTime end = new DateTime(now.Year, now.Month, now.Day + 1, TimeToWakeUp[0], TimeToWakeUp[1], 0);
+
+            TimeSpan duration = end - start;
+
+            if (duration.Hours == 0)
+                Console.WriteLine($"Будильник зазвонит через {duration.Minutes} минут.");
+            else if (duration.Minutes == 0)
+                Console.WriteLine($"Будильник зазвонит через {duration.Hours} час(часов).");
+            else
+                Console.WriteLine($"Будильник зазвонит через {duration.Hours} час(часов) и {duration.Minutes} минут.");
+        }
+    }
+}
+//DateTime start = new DateTime(2025, 3, 22, TimeToBed[0], TimeToBed[1], 0);
+//DateTime end = new DateTime(2025, 3, 23, TimeToWakeUp[0], TimeToWakeUp[1], 0);
+//TimeSpan duration = end - start;
+
+//Console.WriteLine($"Разница: {duration.Hours} часов {duration.Minutes} минут");
